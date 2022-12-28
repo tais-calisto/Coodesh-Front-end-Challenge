0797ffef8460b4793a7faa9f136a0e5b25d8e349
Tais Calisto <taiscalisto97@gmail.com>
2022-12-28 16:57:13 -0300

    Added dropdown

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,24 +1,58 @@
+import { useState } from 'react';
 import unycosLogo from '../../assets/unycos.svg';
 import styles from './Header.module.css';
 
 const Header = () => {
+  const [isLogged, setIsLogged] = useState(false);
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const handleClick = () => {
+    setIsLogged(!isLogged);
+  };
+
+  const handleDropdow = () => {
+    setShowDropdown(!showDropdown);
+  };
+
   return (
     <header className={styles.container}>
       <div>
         <img src={unycosLogo} alt='Unycos Logo'></img>
       </div>
-
       <nav className={styles.navegation}>
         <ul>
           <li>
             <a href='#'>Cursos</a>
           </li>
-          <li>
-            <a href='#'>Register</a>
-          </li>
-          <li>
-            <a href='#'>Log in</a>
-          </li>
+          {isLogged ? (
+            <div className={styles.dropdown}>
+              <li onClick={handleDropdow}>
+                <p>
+                  Hola, <strong>Glaydston</strong>
+                </p>
+              </li>
+              {showDropdown ? (
+                <ul className={styles.dropdwonItems}>
+                  <li>Panel de Control</li>
+                  <li>Tus cursos</li>
+                  <li>Perfil</li>
+                  <li>cuenta</li>
+                  <li onClick={handleClick}>Cerrar sesión</li>
+                </ul>
+              ) : (
+                <></>
+              )}
+            </div>
+          ) : (
+            <>
+              <li>
+                <a href='#'>Register</a>
+              </li>
+              <li onClick={handleClick}>
+                <a href='#'>Log in</a>
+              </li>
+            </>
+          )}
         </ul>
       </nav>
     </header>
