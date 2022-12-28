9ee15a83a2eb6811fabfd91d82a9e11bbae5d806
Tais Calisto <taiscalisto97@gmail.com>
2022-12-28 20:45:00 -0300

    Responsive hero section

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,15 +1,30 @@
 import styles from './Hero.module.css';
+import heroMobile from '../../assets/heroMobile.svg';
 
 const Hero = () => {
   return (
-    <section className={styles.hero}>
-      <div>
-        <h1>Mireia Belmonte</h1>
-        <h2>
-          enseña <strong>natación</strong>
-        </h2>
-      </div>
-    </section>
+    <>
+      <section className={styles.hero}>
+        <div className={styles.content}>
+          <h1>Mireia Belmonte</h1>
+          <h2>
+            enseña <strong>natación</strong>
+          </h2>
+        </div>
+      </section>
+      <section className={styles.heroMobile}>
+        <img src={heroMobile}></img>
+        <div className={styles.content}>
+          <h1>Mireia Belmonte</h1>
+          <h2>
+            enseña <strong>natación</strong>
+          </h2>
+        </div>
+        <div className={styles.btn}>
+          <button>SOLICITAR INFORMACIÓN</button>
+        </div>
+      </section>
+    </>
   );
 };
 
