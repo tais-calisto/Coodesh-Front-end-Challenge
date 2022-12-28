844e040d194c95ece2dc306f7b65afd0d78ead8c
Tais Calisto <taiscalisto97@gmail.com>
2022-12-28 18:14:53 -0300

    Updated header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,61 +1,72 @@
 import { useState } from 'react';
 import unycosLogo from '../../assets/unycos.svg';
 import styles from './Header.module.css';
+import iconChevronDown from '../../assets/chevron-down.svg';
 
 const Header = () => {
   const [isLogged, setIsLogged] = useState(false);
   const [showDropdown, setShowDropdown] = useState(false);
 
-  const handleClick = () => {
-    setIsLogged(!isLogged);
+  const handleLogin = () => {
+    setIsLogged(true);
   };
 
   const handleDropdow = () => {
     setShowDropdown(!showDropdown);
   };
 
+  const handleLogout = () => {
+    setShowDropdown(false);
+    setIsLogged(false);
+  };
+
   return (
-    <header className={styles.container}>
-      <div>
-        <img src={unycosLogo} alt='Unycos Logo'></img>
-      </div>
-      <nav className={styles.navegation}>
-        <ul>
-          <li>
-            <a href='#'>Cursos</a>
-          </li>
-          {isLogged ? (
-            <div className={styles.dropdown}>
-              <li onClick={handleDropdow}>
-                <p>
-                  Hola, <strong>Glaydston</strong>
-                </p>
-              </li>
-              {showDropdown ? (
-                <ul className={styles.dropdwonItems}>
-                  <li>Panel de Control</li>
-                  <li>Tus cursos</li>
-                  <li>Perfil</li>
-                  <li>cuenta</li>
-                  <li onClick={handleClick}>Cerrar sesión</li>
-                </ul>
-              ) : (
-                <></>
-              )}
-            </div>
-          ) : (
-            <>
+    <>
+      <header className={styles.header}>
+        <div className={styles.container}>
+          <div>
+            <img src={unycosLogo} alt='Unycos Logo'></img>
+          </div>
+          <nav className={styles.navegation}>
+            <ul>
               <li>
-                <a href='#'>Register</a>
+                <a href='#'>Cursos</a>
               </li>
-              <li onClick={handleClick}>
-                <a href='#'>Log in</a>
-              </li>
-            </>
-          )}
+              {isLogged ? (
+                <li className={styles.dropdown}>
+                  <div onClick={handleDropdow} className={styles.user}>
+                    <p>
+                      Hola, <strong>Glaydston</strong>
+                    </p>
+                    <img src={iconChevronDown}></img>
+                  </div>
+                </li>
+              ) : (
+                <>
+                  <li>
+                    <a href='#'>Register</a>
+                  </li>
+                  <li onClick={handleLogin}>
+                    <a href='#'>Log in</a>
+                  </li>
+                </>
+              )}
+            </ul>
+          </nav>
+        </div>
+      </header>
+      {showDropdown ? (
+        <ul className={styles.dropdownItems}>
+          <li>Panel de Control</li>
+          <li>Tus cursos</li>
+          <li>Perfil</li>
+          <li>cuenta</li>
+          <li onClick={handleLogout}>Cerrar sesión</li>
         </ul>
-      </nav>
-    </header>
+      ) : (
+        <></>
+      )}
+    </>
   );
 };
 
