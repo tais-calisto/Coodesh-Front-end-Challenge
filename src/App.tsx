b388715c2f80520fa9b51e8200009f5ff3a0a605
Tais Calisto <taiscalisto97@gmail.com>
2023-01-01 12:21:34 -0300

    Responsive articles section

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,6 @@ function App() {
       <Form />
       <Comments />
       <Articles />
-      <Blog />
       <Footer />
     </>
   );
--- src/components/Articles/index.tsx
+++ src/components/Articles/index.tsx
@@ -1,24 +1,30 @@
 import style from './Articles.module.css';
 import { articles } from '../../utils/articles';
+import Blog from '../Blog';
 
 const Articles = () => {
   return (
-    <section className={style.container}>
-      <h2>Artículos Relacionados</h2>
-      <ul className={style.articlesContainer}>
-        {articles.map((a) => (
-          <li key={a._id} className={style.article}>
-            <h3>{a.title}</h3>
-            <p>{a.date}</p>
-          </li>
-        ))}
-      </ul>
-      <div className={style.btn}>
-        <button className='buttonSecondaryWhite'>
-          Leer más en nuestro blog
-        </button>
+    <>
+      <section className={style.container}>
+        <h2>Artículos Relacionados</h2>
+        <ul className={style.articlesContainer}>
+          {articles.map((a) => (
+            <li key={a._id} className={style.article}>
+              <h3>{a.title}</h3>
+              <p>{a.date}</p>
+            </li>
+          ))}
+        </ul>
+        <div className={style.btn}>
+          <button className='buttonSecondaryWhite'>
+            Leer más en nuestro blog
+          </button>
+        </div>
+      </section>
+      <div className={style.blog}>
+        <Blog />
       </div>
-    </section>
+    </>
   );
 };
 
